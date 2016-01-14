708f81fcb7185df4d3945d556d17561264d2dfdb
Tshegofatso Tshehla <tshegofatso100@gmail.com>
2016-01-14 19:47:55 +0200

    getting tshego

--- src/main/java/za/co/lebogang/testing/App.java
+++ src/main/java/za/co/lebogang/testing/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        tshego();
+    }
+    public static void tshego(){
+    	System.out.println("Helo tshegofatso");
     }
 }
